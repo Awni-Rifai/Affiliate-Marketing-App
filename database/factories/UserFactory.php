1ea2ee47259ea0e640962c64c93330b079b2eaa9
awni rifai <awni.rifai1998@gmail.com>
2022-01-21 21:24:10 +0200

    demo2

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,7 +23,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
-            'image' => $this->faker->image('public\storage\images'),
+            'image' => $this->faker->image(),
             'referrer_id'=>2,
             'created_at'=>Carbon::today()->subDays(rand(0, 14)),
 
